04ea3c036c368a808105d7b2d6105eb292929f8f
Mathys Haubert <mathys.haubert80@gmail.com>
2024-03-16 14:14:33 +0100

    fistt url

--- backend/app.py
+++
@@ -1,2 +0,0 @@
-def run():
-    path: str = 'dzdzq'
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
-import app
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    app.run()
+app: FastAPI = FastAPI()
+
+
+@app.get("/getAll")
+def read_root():
+    return {"Hello": "World"}
+
+# exemple of request with parameters
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
