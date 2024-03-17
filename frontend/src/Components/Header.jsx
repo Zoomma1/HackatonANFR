0e3dbb71eecabf8b2928493c2b4ee5ebc597409d
EnzoDEPRE <ende63240@eleve.isep.fr>
2024-03-17 02:04:18 +0100

    oui

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+import BasicExample from "./Navbar";
+import NavScrollExample from "./Navbar";
+import OffcanvasExample from "./Navbar";
+
+export function Header() {
+  return (
+    <Navbar/>
+  );
+}
+
---
+++ frontend/src/Components/Table/TableDisplay.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import { Localisation } from "../../Models/Localisation";
+import { Table, TableContainer, Tbody } from "@chakra-ui/react";
+import { Services } from "../../Models/Services";
+
+interface TableDisplayProps {
+  localisation: Localisation | null;
+  name: string[];
+}
+
+export const TableDisplay: React.FC<TableDisplayProps> = ({
+  localisation,
+  name,
+}) => {
+  const [itemsToShow, setItemsToShow] = useState(20); // Start by showing 20 items
+
+  const loadMoreItems = () => {
+    setItemsToShow(itemsToShow + 20); // Load 20 more items each time
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    loadMoreItems();
+  };
+
+  if (!localisation || name.length === 0) return <></>;
+
+  return (
+    <TableContainer>
+      <Table>
+        <Tbody>
+          <tr>
+            <th>Localisation</th>
+            <th>Service</th>
+            <th>Fréquence min</th>
+            <th>Fréquence max</th>
+            <th>Fréquence prédéfinie</th>
+          </tr>
+          {name.slice(0, itemsToShow).map((name) =>
+            (localisation[name] as Services[]).map((item: Services) => (
+              <tr>
+                <td>{name}</td>
+                <td>{item.Service}</td>
+                <td>{item.Frequence.Fr_min}</td>
+                <td>{item.Frequence.Fr_max}</td>
+                <td>{item.Frequence.Preset}</td>
+              </tr>
+            ))
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
